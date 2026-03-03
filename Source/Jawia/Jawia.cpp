02a62b0828ab9d4661a7e5768860ff97f7004a9b
Krystian <krystian.pawel.robak@gmail.com>
2026-03-03 18:28:49 +0100

    Init

---
+++ Source/Jawia/Jawia.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Jawia.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Jawia, "Jawia" );
